a309a5b2272b6c1275c44397315ca412010ae328
jefferson <jefferson@localhost>
2014-02-18 19:18:22 -0300

    Commit inicial.

---
+++ Questao1/src/br/unipe/mlp/lista1/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package br.unipe.mlp.lista1.hello;
+
+/**
+ * Classe Hello World.
+ * @author jefferson
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World para o GitHub.");
+	}
+	
+}
